aefbe5ac5277d327a0f9d0ac21abe9b6ae7d27c4
Mahiru <sajid.mahi.016@gmail.com>
2026-07-01 04:13:07 -0700

    Create taskone.java

---
+++ labthree/labfour/labfive/taskone.java
@@ -0,0 +1,44 @@
+//Lab Five Task 1
+class BankAccount {
+    int accountNumber;
+    String accountHolderName;
+    double balance;
+
+    // Static variable
+    static String bankName = "Sonali Bank";
+
+    // Constructor
+    BankAccount(int accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    // Method to display account details
+    void displayAccountInfo() {
+        System.out.println("Bank Name: " + bankName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolderName);
+        System.out.println("Balance: " + balance);
+    }
+
+    // Static method to display bank name
+    static void showBankName() {
+        System.out.println("Bank Name: " + bankName);
+    }
+}
+
+// Main Class
+public class BankAccountTest {
+    public static void main(String[] args) {
+        BankAccount account1 = new BankAccount(101, "Istiak Khan", 50000);
+        BankAccount account2 = new BankAccount(102, "Sifat", 70000);
+
+        account1.displayAccountInfo();
+        System.out.println();
+        account2.displayAccountInfo();
+
+        // Calling static method
+        BankAccount.showBankName();
+    }
+}
