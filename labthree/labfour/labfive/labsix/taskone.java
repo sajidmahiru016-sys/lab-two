7f0ad1b37764aef0f4c09301ca77de9849578466
Mahiru <sajid.mahi.016@gmail.com>
2026-07-01 04:15:07 -0700

    Create taskone.java

---
+++ labthree/labfour/labfive/labsix/taskone.java
@@ -0,0 +1,38 @@
+//Lab Six Task 1
+class GrandFather {
+    String name = "Jolil";
+
+    void company() {
+        System.out.println(name + " Owns a company");
+    }
+}
+
+class Father extends GrandFather {
+    public Father() {
+        this.name = "Ismail";
+    }
+
+    void car() {
+        System.out.println(name + " Owns a car");
+    }
+}
+
+class Son extends Father {
+    public Son(String n) {
+        this.name = n;
+    }
+
+    void doesNothing() {
+        System.out.println("Eat sleep Drive Car, Repeat");
+    }
+}
+
+public class InheritanceTest {
+    public static void main(String[] args) {
+        Son son = new Son("Abir");
+
+        son.company();
+        son.car();
+        son.doesNothing();
+    }
+}
